a55e601fda333d5c160b579de50038e832a9f65b
Carghaez <kejan73@gmail.com>
2016-02-24 18:02:35 +0100

    Nuova lezione

---
+++ 2016-02-24/javascript_come_includerlo/script_primodiv.js
@@ -0,0 +1,7 @@
+var div = document.getElementById('primo_div');
+if (div === null)
+{
+	console.log('il primo_div non è ancora stato creato');
+}else{
+	div.textContent = "script_primodiv";
+}
---
+++ 2016-02-24/javascript_come_includerlo/script_secondodiv.js
@@ -0,0 +1,7 @@
+var div = document.getElementById('secondo_div');
+if (div === null)
+{
+	console.log('il secondo_div non è ancora stato creato');
+}else{
+	div.textContent = "script_secondodiv";
+}
